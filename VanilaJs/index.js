518d46f979fd8ee816305984a2132416c4854318
JunYoungJS <ktsdsd94@gmail.com>
2022-03-20 13:33:38 +0900

    [기능구현] html 태그 변수 지정

---
+++ VanilaJs/index.js
@@ -0,0 +1,7 @@
+const bg=document.getElementById('background')
+const backgroundList=["img/1.jpeg","img/2.jpeg","img/3.jpeg","img/4.jpeg"]
+const clocktitle=document.getElementById('clock-title')
+const loginform=document.querySelector('#login-form')
+const todoListform=document.getElementById('todo-form')
+const todoListformMember=document.querySelector('#todo-form h1')
+const todoContentList=document.querySelector('#todo-content')
