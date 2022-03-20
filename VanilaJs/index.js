f161c6a4a3f46c5843e032092701a22bc67846fc
JunYoungJS <ktsdsd94@gmail.com>
2022-03-20 13:36:36 +0900

    [기능구현] 회원 로그인 기능

--- VanilaJs/index.js
+++ VanilaJs/index.js
@@ -14,3 +14,43 @@ const changeBackGround=()=>{
 }
 setInterval(changeBackGround,5000) 
 // // 5초마다 배경화면이 바뀜
+
+
+// 2. 실시간 시계
+const showCurrentClock=()=>{
+    const date=new Date()
+    const currentHour=date.getHours().toString().padStart(2,"0")
+    const currentMinute=date.getMinutes().toString().padStart(2,"0")
+
+    const currentSecond=date.getSeconds().toString().padStart(2,"0")
+
+
+    clocktitle.innerHTML=`${currentHour}시 ${currentMinute}분 ${currentSecond}초`
+}
+setInterval(showCurrentClock,1000)
+
+
+// 3. 로그인 부분 구현 (로컬스토리지 사용 )
+
+const loginbtnClick=(e)=>{
+    e.preventDefault();
+
+    const username=document.getElementById('username').value
+    const pwd=document.getElementById('pwd').value
+
+    if(localStorage.getItem(`${username}`) && localStorage.getItem(`${pwd}`)){
+        alert('회원입니다. 어서오세요')
+        loginform.style.display="none";
+        todoListform.style.display="block"
+        todoListformMember.innerHTML=`${username}님의 할일`
+    }
+    else{
+        localStorage.setItem(`${username}`,pwd)
+        alert('회원가입이 완료되었습니다!')
+        loginform.style.display="none";
+        todoListform.style.display="block"
+        todoListformMember.innerHTML=`${username}님의 할일`
+        
+    }
+
+}
