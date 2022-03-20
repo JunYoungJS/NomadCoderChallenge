703b50cb6e32607f2d2e292e0a49a5a14e25cf24
JunYoungJS <ktsdsd94@gmail.com>
2022-03-20 13:34:26 +0900

    [기능구현]5초마다 랜덤 배경화면지정

--- VanilaJs/index.js
+++ VanilaJs/index.js
@@ -5,3 +5,12 @@ const loginform=document.querySelector('#login-form')
 const todoListform=document.getElementById('todo-form')
 const todoListformMember=document.querySelector('#todo-form h1')
 const todoContentList=document.querySelector('#todo-content')
+
+
+const changeBackGround=()=>{
+    const bgImgNumber=Math.floor(Math.random() * backgroundList.length);
+    const bgImg=`url(${backgroundList[bgImgNumber]})`
+    bg.style.backgroundImage=bgImg;
+}
+setInterval(changeBackGround,5000) 
+// // 5초마다 배경화면이 바뀜
